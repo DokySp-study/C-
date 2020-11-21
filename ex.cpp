5fbd36d6b0ab8a8bed46b7d68f47b97b22e0e44c
Tony Dokyoon Kim <uhug@naver.com>
2020-11-21 04:37:22 +0000

    프린트 함수 추가

--- ex.cpp
+++ ex.cpp
@@ -27,11 +27,48 @@ class Train {
 
 
 
-int main() {
-  char *str = "가나다";
-  std::cout << "==================================================" << std::endl; //50개
-  std::cout << str ;
 
+
+void printHead(std::string input, int width){
+  int length = input.length()/3;
+	
+	for(int i=0; i<width; i++)
+		std::cout << "=";
+  std::cout << std::endl;
+	
+	for(int i=0; i<width/2 - length; i++) 
+		std::cout << " ";
+	std::cout << input << std::endl;
+	
+return;
+}
+
+void printContext(std::string context, int width){
+	int length = context.length()/3;
+		for(int i=0; i<width/2 - length; i++) 
+		std::cout << " ";
+	std::cout << context << std::endl;
+}
+
+
+
+int main() {
+
+	std::string aa;
+	
+printHead("메뉴 번호를 선택하세요", 40);
+	
+	printContext("", 40);
+	printContext("1. 열차 예매하기", 40); // 고객정보, 표 정보, 열차 정보 신규 입력
+	
+	// 위에 입력된 정보를 바탕으로 정보를 조회함
+	printContext("2. 열차 조회", 40);
+	printContext("3. 고객번호로 조회하기", 40);
+	printContext("4. 열차표로 조회하기", 40);
+	
+	printContext("", 40);
+	std::cin >> aa;
+	
   // 한글과 영어 구분하는 코드
   // String 관련 클래스 
   
